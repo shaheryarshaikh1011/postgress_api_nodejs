da34ff8722969ce11fb1f40d2a60e618dc0f4eb4
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2021-04-04 06:28:43 +0530

    make it better3

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ app.post("/api/emp", async(req,res)=>{
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
